423f7e44dc07cbb5cad2e87966b51b418be4439c
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2021-03-03 09:29:45 -0600

    Create teams_mapping spider

---
+++ nba/nba/spiders/teams_mapping.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class TeamsMappingSpider(scrapy.Spider):
+    name = 'teams_mapping'
+    allowed_domains = ['basketball-reference.com']
+    start_urls = ['http://basketball-reference.com/teams/']
+
+    def parse(self, response):
+        ths = response.xpath('//th[@data-stat="franch_name"]')
+        for th in ths:
+            a = th.xpath('./a')
+            if a:
+                team_name = a.xpath('./text()').extract_first()
+                href = (a.xpath('./@href').extract_first()
+                    .replace('/teams/', '')
+                    .replace('/', '')
+                )
+                yield dict(team_name=team_name, href=href)
