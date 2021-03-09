e3373dbea61f6f366fe6d18e70796901e969eef8
Brendan Fitzpatrick <brendanjfitzpatrick0@gmail.com>
2021-03-09 10:21:41 -0600

    Create MongoDBPipeline class

--- nba/nba/pipelines.py
+++ nba/nba/pipelines.py
@@ -6,8 +6,33 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from pymongo import MongoClient
+from os import environ
 
 
-class NbaPipeline:
+class MongoDBPipeline:
+
+    CONFIG = {
+        'host': environ['SCRAPY_MONGODB_SERVER'],
+        'port': int(environ['SCRAPY_MONGODB_PORT']),
+        'db': environ['SCRAPY_MONGODB_DATABASE'],
+    }
+
+    def open_spider(self, spider):
+        self.client = MongoClient(
+            host=self.CONFIG['host'],
+            port=self.CONFIG['port']
+        )
+        self.db = self.client[self.CONFIG['db']]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        data = ItemAdapter(item).asdict()
+        update = {"$set": data}
+        # perform upsert
+        self.db[spider.name].update_one(filter=data, update=update, upsert=True)
+
         return item
+    
